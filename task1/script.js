d22ec8492140a66e3579a8fb15c5eb220d317f76
AshrafiKhan <pathanashrafi2709@gmail.com>
2024-03-04 11:07:11 +0530

    Created Level 1 Directory, Moved Task 1 and 2 into it

--- task1/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerButton = document.querySelector('.menu-box');
-    const navMenu = document.querySelector('.right-header');
-
-    hamburgerButton.addEventListener('click', function () {
-        navMenu.classList.toggle('show');
-    });
-});
